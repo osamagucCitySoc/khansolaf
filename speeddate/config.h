e8b6b5f2580de686a1c02b0c058074bbc10846c3
Osama Rabie <osamarabie@Osamas-MacBook-Pro.local>
2015-01-20 15:33:37 +0400

    Changed FB, Google admob, Parse and In App.

---
+++ speeddate/config.h
@@ -0,0 +1,42 @@
+//
+//  config.h
+
+//
+//  Created by STUDIO76 on 03.07.14.
+//  Copyright (c) 2014 studio76. All rights reserved.
+//
+
+
+///// link download you app
+#define AppUrl                    @"http://studio76.pro"
+
+//// on/off animation menu
+#define animationMenu             @"YES"  // YES or NO
+
+/// limit view user if no vip member
+#define limitQueruNoVipUser        9;
+
+
+/////////// GoogleAd SETTING  //////////////////////////////
+#define kSampleAdUnitID           @"ca-app-pub-2433238124854818/6884361194"
+
+/// url terms you app
+#define urlTerms                  @"http://q8app.net/privacy/"
+
+/// url to you additional app
+#define MyAppUrl                  @"https://itunes.apple.com/us/app/dyfny-adyfk/id663980256?ls=1&mt=8"
+
+/// config in app purchase
+
+#define subscribe1mth            @"arabdevs.khansolaf.monthOne"
+#define subscribe3mth            @"arabdevs.khansolaf.monthThree"
+#define subscribe6mth            @"arabdevs.khansolaf.monthSix"
+#define subscribe1yar            @"arabdevs.khansolaf.yearOne"
+
+//parse config
+
+#define appIdparse               @"MSVqnKHTmQNMNab4VqY5eNt7DzAMoY7W85PEFIz2"
+#define appKeyparse              @"B0IBzM1NOfk2YyHRtm5ulZPmbGxqTpz8df3Ppize"
+
+
+
